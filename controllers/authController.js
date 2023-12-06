c61b0a0d8307f99391c63cdbd6e3f6920372f495
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-12-06 17:38:15 +0300

    handling signup errors and displaying them in form

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,44 +1,41 @@
 const User = require('../models/User')
+const jwt = require('json-web-token')//pkg to create token
 
-//the parent err look like this..simplified
-/*{
-    errors: {
-        email: ValidateError...{
-            properties:{...}
-            ...
-        },
-        password: ValidateError...{
-            properties:{...}
-            ...
-        }
-    }
-}*/
+//JWT TOKENS
+//an encoded long list of char
+//we'll send it to browser to store it in a cookie 
+//this cookies we'll be sent to server for every request in Browser
+//tokens have 3 parts (i)header(ii)payload(iii)signature(check jwt.io)
+//to create signature header and payload are encoded then hashed together with a secret Str
+//the signature then added at the end of jwt token.
+//when cookies with jwt are received by server,
+//it takes the encoded header & payload & hashes 'em with secret Str in server,
+//if hashed result matches signature in jwt then token is valid & untampered 
+
+
+//func to create jwt token & store in a cookie
+//we'll use id from mongodb as payload below..headers used automatically
+const maxAge = 3*24*60*60 //maxAge vals measured in seconds in jwt
+const createToken = (id) => {
+    //we want token to expire after 3 days
+    //jwt.sign takes 3 args: the payload obj,secret str & options Obj
+    return jwt.sign({id},'my secret string',{ expiresIn: maxAge})
+}
+
 
 //handling errors...outside the signup_post func(cleaner code)
 const handleErrors = (err) => {
-    //err.code undefined except when similar email used(unique)(check terminal)
     console.log(err.message, err.code)//in terminal
     let errors = { email: "", password: ""}
-    //err.code is 11000(logged to terminal when same email used)
     if(err.code === 11000){
         errors.email = 'Email is already registered'
     }
-
-    //err.message has start of Str that's unchanging:-'user validation failed' 
-    //we check if its an err message using that unchanging part
     if(err.message.includes('user validation failed')){
-        //parent err obj is HUGE  with nested objs(check above or in terminal)
-        //we need properties obj in err.errors(errors a nested obj)
-        //turn errors obj to arr(using 'Object.values') to loop each obj to get properties
-        //destructuring {properties}..wrap in new set of brackets ()
-        // Object.values(err.errors).forEach(error.properties => {
         Object.values(err.errors).forEach(({ properties }) => {
-            //another way to update errors obj vals defined above in let
-            //if path is email then errors.email is updated with message in that properties Obj
             errors[properties.path] = properties.message;
         })
     }
-    return errors;//when func is run this returned
+    return errors;
 }
 
 //exporting funcs during creation(can also export below)
@@ -48,14 +45,17 @@ module.exports.signup_get = (req, res) => {
 
 module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
-
+    //we'll create jwt token & store in cookie
+    //res.cookie takes 3 args, cookie name,cookie value(token) & options obj to set properties
+    //maxAge unit stored in milliseconds in a cookie-maxAge defined at top 
     try {
         const user = await User.create({ email, password })
-        res.status(201).json(user)
+        // const token = createToken(user._id)//user._id(mongo doc) is gotten from above
+        // res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge*1000})
+        res.status(201).json({user: user.id})//passed onto frontend...returned by fetch in const result(check signup.ejs) 
     } catch (err) {
-        //store func in var so returned values are in the var
         const errors = handleErrors(err);
-        res.status(400).json({errors})
+        res.status(400).json({errors})//passed onto frontend
     }
 }
 
